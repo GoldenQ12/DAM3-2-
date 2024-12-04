610e4ab534e735a4c4fd3233ca69627a2fb43137
GoldenQ12 <goldenytstorm@gmail.com>
2024-12-04 08:17:33 +0100

    #37 - Design Commit

---
+++ Evaluacion 2/VIajesAgencia/Viajes.vb
@@ -0,0 +1,29 @@
+﻿Public Class Viajes
+    Private Sub Viajes_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.CenterToScreen()
+    End Sub
+
+    Private Sub BtnClientes_Click(sender As Object, e As EventArgs) Handles btnClientes.Click
+        Me.Hide()
+        Clientes.Show()
+    End Sub
+
+    Private Sub btnBack_Click(sender As Object, e As EventArgs) Handles btnBack.Click
+        Me.Close()
+    End Sub
+
+    Private Sub BtnDestinos_Click(sender As Object, e As EventArgs) Handles btnDestinos.Click
+        Me.Hide()
+        Destinos.Show()
+    End Sub
+
+    Private Sub btnViajes_Click(sender As Object, e As EventArgs) Handles btnViajes.Click
+        Me.Hide()
+        ViajesRegistros.Show()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Hide()
+        ViajesPorNombre.Show()
+    End Sub
+End Class
